ab39e3c13e7f01ec0151c178ad059e3bbd819565
Daniil Panov <daniil_panov2005@mail.ru>
2025-10-28 23:35:01 +0300

    fix[api]: dialogues and choices

--- api/actions.php
+++ api/actions.php
@@ -53,75 +53,89 @@ function getActionsChain($pdo, $scene_id, $choice_alias = null) {
     $stmt->execute();
     $scene_id = (int) $stmt->fetchColumn();
 
-    $start_id = null;
-    if ($choice_alias !== null) {
-        $query = "
-            SELECT id
-            FROM actions
-            WHERE actions.scene_id = :scene_id AND actions.choice_alias = :choice_alias
-        ";
-        $stmt = $pdo->prepare($query);
-        $stmt->bindValue(':scene_id', $scene_id, PDO::PARAM_INT);
-        $stmt->bindValue(':choice_alias', $choice_alias);
-        $stmt->execute();
-        $start_id = (int) $stmt->fetchColumn();
-    }
-
-    /*$query = "
-        WITH RECURSIVE get_action_chains (
-            SELECT id, parent_id, scene_id, 'action', choice_text, choice_alias, has_choice
-            FROM actions
-            WHERE scene_id = :scene_id" . ($scene_id ? " AND id = :start_id" : "") . "
-
-            UNION ALL
-
-            SELECT id, parent_id, scene_id, 'action', choice_text, choice_alias, has_choice
-            FROM actions
-            WHERE scene_id = :scene_id AND
-        )
-
-    ";*/
+    $needle_actions = [];
     $query = "
-      SELECT id, parent_id, scene_id, `action`, choice_text, choice_alias, has_choice
-      FROM actions
-      WHERE scene_id = :scene_id
+        SELECT id, parent_id, scene_id, `action`, choice_text, choice_alias, has_choice
+        FROM actions
+        WHERE actions.scene_id = :scene_id AND 
+    " . ($choice_alias ? "actions.choice_alias = :choice_alias" : "actions.choice_alias IS NULL");
+    $stmt = $pdo->prepare($query);
+    $stmt->bindValue(':scene_id', $scene_id, PDO::PARAM_INT);
+    if ($choice_alias)
+        $stmt->bindValue(':choice_alias', $choice_alias);
+    $stmt->execute();
+
+    $first_action = $stmt->fetch();
+    $first_action['action'] = json_decode($first_action['action'], true);
+    $parent_id = $first_action['id'];
+    $needle_actions[] = $first_action;
+
+    $query = "
+        SELECT id, parent_id, scene_id, `action`, choice_text, choice_alias, has_choice
+        FROM actions
+        WHERE actions.scene_id = :scene_id AND actions.id != :parent_id
     ";
     $stmt = $pdo->prepare($query);
     $stmt->bindValue(':scene_id', $scene_id, PDO::PARAM_INT);
+    $stmt->bindValue(':parent_id', $parent_id, PDO::PARAM_INT);
     $stmt->execute();
     $actions = $stmt->fetchAll();
 
-    $needle_actions = [];
-    $found_choice = false;
-    $found_next_choice = false;
+    $count_actions = 0;
+    $all_actions = count($actions);
+
+    while ($count_actions < $all_actions) {
+        $key = null;
+        $found = false;
+
+        foreach ($actions as $key => $action) {
+            if ($parent_id !== $action['parent_id'])
+                continue;
+
+            $action['action'] = json_decode($action['action'], true);
+            $needle_actions[] = $action;
+            ++$count_actions;
+            $found = true;
+            $parent_id = $action['id'];
+
+            if ($action['has_choice'])
+                break(2);
+
+            break;
+        }
+
+        if (!$found)
+            break;
+
+        unset($actions[$key]);
+    }
+
+    $last_action = $needle_actions[count($needle_actions) - 1];
+
+    if (!$last_action['has_choice']) {
+        echo json_encode([
+            'success' => true,
+            'actions' => $needle_actions,
+        ]);
+
+        return;
+    }
+
+    $query = "
+            SELECT choice_alias, choice_text
+            FROM actions
+            WHERE actions.scene_id = :scene_id AND actions.parent_id = :parent_id
+        ";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindValue(':scene_id', $scene_id, PDO::PARAM_INT);
+    $stmt->bindValue(':parent_id', $last_action['id'], PDO::PARAM_INT);
+    $stmt->execute();
+    $choices_actions = $stmt->fetchAll();
+
     $choice_variants = [];
 
-    foreach ($actions as $action) {
-        if ($start_id !== null) {
-            if ($action['id'] === $start_id)
-                $start_id = null;
-            else
-                continue;
-        }
-
-        if ($found_next_choice) {
-            if (!$action['choice_alias'])
-                break;
-
-            $choice_variants[$action['choice_alias']] = $action['choice_text'];
-            continue;
-        }
-
-        if (!$found_choice && $choice_alias && $action['choice_alias'] !== $choice_alias)
-            continue;
-
-        $found_choice = true;
-        $action['action'] = json_decode($action['action'], true);
-        $needle_actions[] = $action;
-
-        if ($action['has_choice'])
-            $found_next_choice = true;
-    }
+    foreach ($choices_actions as $choices_action)
+        $choice_variants[$choices_action['choice_alias']] = $choices_action['choice_text'];
 
     echo json_encode([
         'success' => true,
