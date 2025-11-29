04e1729a0c7d7f8e08457335cef212a35ef3d597
Daniil Panov <daniil_panov2005@mail.ru>
2025-11-29 15:17:29 +0300

    Use Utils.fetchJson

--- js/api.js
+++ js/api.js
@@ -1,26 +1,13 @@
-async function fetchJson(url, method = 'get', body = undefined, timeout = 3000) {
-    const controller = new AbortController();
-    const id = setTimeout(() => controller.abort(), timeout);
-
-    return await fetch(`api/${url}`, {
-        method,
-        body,
-        signal: controller.signal,
-    })
-        .then(response => response.json())
-        .finally(() => clearTimeout(id));
-}
-
 class GameAPI {
     constructor(id) {
         this.id = id;
     }
 
     static getAll() {
-        return fetchJson('/games');
+        return Utils.fetchJson('/games');
     }
 
     getInfoById() {
-        return fetchJson(`/games/${this.id}`);
+        return Utils.fetchJson(`/games/${this.id}`);
     }
 }
