a223591ac01ee0c49acd3f5284c1457724b0d68e
Daniil Panov <daniil_panov2005@mail.ru>
2026-01-18 12:29:17 +0300

    js/scene: make controller

--- js/scene.js
+++ js/scene.js
@@ -1,20 +1,30 @@
-class SceneView {
-    currentAction = undefined;
+class SceneController {
+    currentActionsGroupKeyIndex = -1;
 
-    constructor(sceneKey, descriptor) {
+    constructor(sceneKey, descriptor, actionsHandler) {
         this.sceneKey = sceneKey;
-        this.parseDescriptor(descriptor);
+        this.actionsHandler = actionsHandler;
+
+        const descriptorObject = toml.parse(descriptor);
+        this.groupsKeys = Object.keys(descriptorObject).sort();
+        this.groups = this.groupsKeys.map(groupKey => parseGroup(descriptorObject[groupKey]));
     }
 
-    parseDescriptor(descriptor) {
-
+    doNextActionsGroup() {
+        return this.doActionsGroup(this.currentActionsGroupKeyIndex + 1);
     }
 
-    async nextAction() {
-        if (this.currentAction && !this.currentAction.finished)
-            return await this.currentAction.finishAction();
-
-        this.currentAction = this.actions.pop();
-        await this.currentAction.doAction();
+    doActionsGroup(keyIdx) {
+        this.currentActionsGroupKeyIndex = keyIdx;
+        this.groups[this.groupsKeys[keyIdx]].map(this.actionsHandler);
     }
+}
+
+const parseGroup = group => Object.keys(group).map(key => parseAction(key, group[key]));
+
+function parseAction(key, mainArg) {
+    const { actionKey, stringArgs } = key.split("(", 2);
+    const args = stringArgs.slice(0, -1).split(",").map(arg => arg.trim());
+
+    return { actionKey, mainArg, args };
 }
