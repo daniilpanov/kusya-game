b3a07e2873e12c62ba7d215ac1a4ce5dd557ef53
Daniil Panov <daniil_panov2005@mail.ru>
2026-01-25 16:01:49 +0300

    js/utils: move typer to the lib

---
+++ js/lib/typer/typer.js
@@ -0,0 +1,39 @@
+class TextTyper {
+    typerProcessId = undefined;
+    finished = false;
+
+    constructor(element, text, delay) {
+        this.element = element;
+        this.text = text;
+        this.delay = delay;
+    }
+
+    startTyping() {
+        if (this.typerProcessId)
+            return;
+
+        this.element.innerHTML = '';
+        let index = 0;
+
+        const type = () => {
+            if (index < this.text.length) {
+                const char = this.text.charAt(index);
+                this.element.innerHTML += char === ' ' ? '&nbsp;' : char;
+                ++index;
+                this.typerProcessId = setTimeout(type, this.delay);
+            } else
+                this.finished = true;
+        }
+
+        type();
+    }
+
+    endTyping() {
+        if (!this.typerProcessId)
+            return;
+
+        clearTimeout(this.typerProcessId);
+        this.element.innerHTML = this.text;
+        this.finished = true;
+    }
+}
