a8e35e19b09c524e09fc5ea7da781aea0828bdb3
Daniil Panov <daniil_panov2005@mail.ru>
2026-01-26 12:31:33 +0300

    js/game: release all actions

--- js/game.js
+++ js/game.js
@@ -2,13 +2,24 @@ const handlersMap = {
     action_setBackground(bgKey) {
         bgKey = String(bgKey);
 
+        this.bgWrapper.innerHTML = "";
+        this.bgWrapper.appendChild(this.backgrounds[bgKey].img);
     },
     action_if(groupKey, [ condition ]) {
         if (this.expressionsParser.evaluate(condition))
             return handlersMap.action_goto.bind(this)(groupKey);
     },
+    action_cloneVar(oldVarName, [ newVarName ]) {
+        this.variables[newVarName] = this.variables[oldVarName];
+    },
     action_setVar(value, [ varName ]) {
         this.variables[varName] = value;
+    },
+    action_addVar(value, [ varName ]) {
+        if (this.variables[varName] === "undefined")
+            return handlersMap.action_setVar.bind(this)(...arguments);
+
+        this.variables[varName] += value;
     },
     action_addStats(value, [ statName ]) {
         this.stats[statName] += value;
@@ -19,23 +30,74 @@ const handlersMap = {
     action_gotoNext() {
         return this.sceneController.doNextActionsGroup();
     },
-    action_showChoice(variants, choiceKey) {
+    action_showPersonSprite(personSprite, [ hideAllOther = false ]) {
+        if (!personSprite) {
+            if (hideAllOther)
+                this.activePersons.forEach(person => person.hide());
 
+            return;
+        }
+
+        const [ personId, personSpriteId = "default" ] = personSprite.split(".");
+        const currentPersonObj = this.persons[personId];
+
+        if (hideAllOther)
+            for (const person of this.activePersons)
+                if (person.name !== currentPersonObj.name)
+                    person.hide();
+
+        currentPersonObj.show(personSpriteId);
+        this.activePersons.push(currentPersonObj);
+
+        return currentPersonObj;
     },
-    action_showPhrase(phrase, [ character, pseudoName ]) {
+    action_showChoice(variants, [ choiceKey, personSprite, pseudoName, hideAllOther = true ]) {
+        this.templateWrappers.choices?.classList.add("active");
+        this.templateWrappers.dialog?.classList.remove("active");
 
+        const personObj = handlersMap.action_showPersonSprite.bind(this)(personSprite, [ hideAllOther ]);
+        const author = pseudoName ?? personObj?.name ?? "...";
+        const text = variants[0];
+
+        const choicesList = variants.slice(1).map(variant => ({
+            "event-choice-id": choiceKey,
+            "event-choice-variant": variant,
+            "content": variant,
+        }));
+
+        this.templates.choices?.render({ author, text, choicesList });
+    },
+    action_showPhrase(text, [ personSprite, pseudoName, hideAllOther = true ]) {
+        this.templateWrappers.choices?.classList.remove("active");
+        this.templateWrappers.dialog?.classList.add("active");
+
+        const personObj = handlersMap.action_showPersonSprite.bind(this)(personSprite, [ hideAllOther ]);
+        const author = pseudoName ?? personObj?.name ?? "...";
+
+        this.templates.dialog?.render({ text, author });
     },
     action_showTitle(title) {
+        return new Promise(r => {
+            this.templateWrappers.sceneTitle?.classList.remove("inactive");
+            this.templates.sceneTitle?.render({ title });
 
+            setTimeout(() => {
+                this.templateWrappers.sceneTitle?.classList.add("inactive");
+                const res = handlersMap.action_gotoNext.bind(this)();
+                return res instanceof Promise ? res.then(r) : r(res);
+            }, 1000);
+        });
     },
     action_gotoNextScene() {
-        return this.loadSceneByKeyIndex(this.currentSceneKeyIndex + 1);
+        this.templateWrappers.dialog?.classList.remove("active");
+        this.templateWrappers.choices?.classList.remove("active");
+        return this.loadSceneByKeyIndex(this.currentSceneKeyIndex + 1).then(() => this.start());
     },
     action_gotoScene(sceneKey) {
         return this.loadSceneByKey(sceneKey);
     },
     action_end() {
-
+        window.location.href = `/?finished=${this.gameResource}`;
     },
 };
 
@@ -44,8 +106,10 @@ class Game {
 
     sortedSceneKeys = undefined;
     sceneDescriptors = undefined;
-    currentSceneKeyIndex = undefined;
     sceneController = undefined;
+
+    currentSceneKeyIndex = undefined;
+    activePersons = [];
 
     stats = undefined;
     persons = undefined;
