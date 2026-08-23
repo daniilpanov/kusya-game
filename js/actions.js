daniilpanov/kusya-game

commit ec8b3f7f86b7f9e6ea09dce15c0cd1d40687da9c

js: add 'handlersMap' fabric and move it to the 'actions.js', automatically binding 'this' in the constructor of the 'Game' class

js/actions.js
@@ -1,0 +1,108 @@
+const createHandlersMap = () => ({
+    action_setBackground(bgKey) {
+        bgKey = String(bgKey);
+
+        this.bgWrapper.innerHTML = "";
+        this.bgWrapper.appendChild(this.backgrounds[bgKey].img);
+    },
+    action_if(groupKey, [ condition ]) {
+        if (this.expressionsParser.evaluate(condition))
+            return this.handlersMap.action_goto(groupKey);
+    },
+    action_cloneVar(oldVarName, [ newVarName ]) {
+        this.variables[newVarName] = this.variables[oldVarName];
+    },
+    action_setVar(value, [ varName ]) {
+        this.variables[varName] = value;
+    },
+    action_addVar(value, [ varName ]) {
+        if (this.variables[varName] === "undefined")
+            return this.handlersMap.action_setVar(...arguments);
+
+        this.variables[varName] += value;
+    },
+    action_addStats(value, [ statName ]) {
+        this.stats[statName].value += value;
+    },
+    action_goto(groupKey) {
+        return this.doActionsGroupByKey(String(groupKey));
+    },
+    action_gotoNext() {
+        return this.doNextActionsGroup();
+    },
+    action_showPersonSprite(personSprite, [ hideAllOther = false ]) {
+        if (!personSprite) {
+            if (hideAllOther)
+                this.activePersons.forEach(person => person.hide());
+
+            return;
+        }
+
+        const [ personId, personSpriteId = "default" ] = personSprite.split(".");
+        const currentPersonObj = this.persons[personId];
+
+        if (hideAllOther)
+            for (const person of this.activePersons)
+                if (person.name !== currentPersonObj.name)
+                    person.hide();
+
+        currentPersonObj.show(personSpriteId);
+        this.activePersons.push(currentPersonObj);
+
+        return currentPersonObj;
+    },
+    action_showChoice(variants, [ choiceKey, personSprite, pseudoName, hideAllOther = true ]) {
+        this.needClickButton = true;
+        this.templateWrappers.choices?.classList.add("active");
+        this.templateWrappers.dialog?.classList.remove("active");
+
+        const personObj = this.handlersMap.action_showPersonSprite(personSprite, [ hideAllOther ]);
+        const author = pseudoName ?? personObj?.name ?? "...";
+        const text = variants[0];
+
+        const choicesList = variants.slice(1).map(variant => ({
+            "event-choice-id": choiceKey,
+            "event-choice-variant": variant,
+            "content": variant,
+        }));
+
+        this.templates.choices?.render({ author, text, choicesList });
+    },
+    action_showPhrase(text, [ personSprite, pseudoName, hideAllOther = true ]) {
+        this.templateWrappers.choices?.classList.remove("active");
+        this.templateWrappers.dialog?.classList.add("active");
+
+        const personObj = this.handlersMap.action_showPersonSprite(personSprite, [ hideAllOther ]);
+        const author = pseudoName ?? personObj?.name ?? "...";
+
+        this.templates.dialog?.render({ text, author });
+    },
+    action_showTitle(title) {
+        return new Promise(r => {
+            this.templateWrappers.sceneTitle?.classList.remove("inactive");
+            this.templates.sceneTitle?.render({ title });
+
+            this.activeTitleTimeoutId = setTimeout(() => {
+                if (!this.activeTitleTimeoutId)
+                    return;
+
+                this.templateWrappers.sceneTitle?.classList.add("inactive");
+                this.activeTitleTimeoutId = undefined;
+
+                const res = this.handlersMap.action_gotoNext();
+                return res instanceof Promise ? res.then(r) : r(res);
+            }, 5000);
+        });
+    },
+    action_gotoNextScene() {
+        this.templateWrappers.dialog?.classList.remove("active");
+        this.templateWrappers.choices?.classList.remove("active");
+        return this.loadSceneByKeyIndex(this.currentSceneKeyIndex + 1).then(() => this.doNextActionsGroup());
+    },
+    action_gotoScene(sceneKey) {
+        return this.loadSceneByKey(sceneKey);
+    },
+    action_end() {
+        window.location.href = `/?finished=${this.gameResource}`;
+    },
+});

game.html
@@ -55,6 +55,7 @@
 <script src="js/utils.js"></script>
 <script src="js/person.js"></script>
 <script src="js/scene.js"></script>
+<script src="js/actions.js"></script>
 <script src="js/game.js"></script>
 
 <script>

js/game.js
@@ -1,112 +1,3 @@
-const handlersMap = {
-    action_setBackground(bgKey) {
-        bgKey = String(bgKey);
-
-        this.bgWrapper.innerHTML = "";
-        this.bgWrapper.appendChild(this.backgrounds[bgKey].img);
-    },
-    action_if(groupKey, [ condition ]) {
-        if (this.expressionsParser.evaluate(condition))
-            return handlersMap.action_goto.bind(this)(groupKey);
-    },
-    action_cloneVar(oldVarName, [ newVarName ]) {
-        this.variables[newVarName] = this.variables[oldVarName];
-    },
-    action_setVar(value, [ varName ]) {
-        this.variables[varName] = value;
-    },
-    action_addVar(value, [ varName ]) {
-        if (this.variables[varName] === "undefined")
-            return handlersMap.action_setVar.bind(this)(...arguments);
-
-        this.variables[varName] += value;
-    },
-    action_addStats(value, [ statName ]) {
-        this.stats[statName].value += value;
-    },
-    action_goto(groupKey) {
-        return this.doActionsGroupByKey(String(groupKey));
-    },
-    action_gotoNext() {
-        return this.doNextActionsGroup();
-    },
-    action_showPersonSprite(personSprite, [ hideAllOther = false ]) {
-        if (!personSprite) {
-            if (hideAllOther)
-                this.activePersons.forEach(person => person.hide());
-
-            return;
-        }
-
-        const [ personId, personSpriteId = "default" ] = personSprite.split(".");
-        const currentPersonObj = this.persons[personId];
-
-        if (hideAllOther)
-            for (const person of this.activePersons)
-                if (person.name !== currentPersonObj.name)
-                    person.hide();
-
-        currentPersonObj.show(personSpriteId);
-        this.activePersons.push(currentPersonObj);
-
-        return currentPersonObj;
-    },
-    action_showChoice(variants, [ choiceKey, personSprite, pseudoName, hideAllOther = true ]) {
-        this.needClickButton = true;
-        this.templateWrappers.choices?.classList.add("active");
-        this.templateWrappers.dialog?.classList.remove("active");
-
-        const personObj = handlersMap.action_showPersonSprite.bind(this)(personSprite, [ hideAllOther ]);
-        const author = pseudoName ?? personObj?.name ?? "...";
-        const text = variants[0];
-
-        const choicesList = variants.slice(1).map(variant => ({
-            "event-choice-id": choiceKey,
-            "event-choice-variant": variant,
-            "content": variant,
-        }));
-
-        this.templates.choices?.render({ author, text, choicesList });
-    },
-    action_showPhrase(text, [ personSprite, pseudoName, hideAllOther = true ]) {
-        this.templateWrappers.choices?.classList.remove("active");
-        this.templateWrappers.dialog?.classList.add("active");
-
-        const personObj = handlersMap.action_showPersonSprite.bind(this)(personSprite, [ hideAllOther ]);
-        const author = pseudoName ?? personObj?.name ?? "...";
-
-        this.templates.dialog?.render({ text, author });
-    },
-    action_showTitle(title) {
-        return new Promise(r => {
-            this.templateWrappers.sceneTitle?.classList.remove("inactive");
-            this.templates.sceneTitle?.render({ title });
-
-            this.activeTitleTimeoutId = setTimeout(() => {
-                if (!this.activeTitleTimeoutId)
-                    return;
-
-                this.templateWrappers.sceneTitle?.classList.add("inactive");
-                this.activeTitleTimeoutId = undefined;
-
-                const res = handlersMap.action_gotoNext.bind(this)();
-                return res instanceof Promise ? res.then(r) : r(res);
-            }, 5000);
-        });
-    },
-    action_gotoNextScene() {
-        this.templateWrappers.dialog?.classList.remove("active");
-        this.templateWrappers.choices?.classList.remove("active");
-        return this.loadSceneByKeyIndex(this.currentSceneKeyIndex + 1).then(() => this.doNextActionsGroup());
-    },
-    action_gotoScene(sceneKey) {
-        return this.loadSceneByKey(sceneKey);
-    },
-    action_end() {
-        window.location.href = `/?finished=${this.gameResource}`;
-    },
-};
-
 class Game {
     lang = "RU";
 
@@ -131,6 +22,10 @@ class Game {
         this.gameResource = gameResource;
         this.descriptorUri = descriptorUri;
         this.variables = {};
+
+        this.handlersMap = Object.fromEntries(
+            Object.entries(createHandlersMap()).map(([key, func]) => ([key, func.bind(this)])),
+        );
 
         const getFromContextCallback = varName => varName.startsWith("stats.") ? this.stats[varName.slice(6)].value : this.variables[varName];
         this.expressionsParser = new ExpressionsParser({ getFromContextCallback });
@@ -247,7 +142,7 @@ class Game {
     }
 
     handleAction({ actionKey, mainArg, args }) {
-        return handlersMap[`action_${actionKey}`].bind(this)(mainArg, args);
+        return this.handlersMap[`action_${actionKey}`](mainArg, args);
     }
 
     _isActiveBackgroundProcesses() {

commit 1fc351e99d51230cd5ace2e406ea32e7130f61f6

js/actions: fix incorrect undefined check in addVar
Fixes: ec8b3f7f86b7f9e6ea09dce15c0cd1d40687da9c

js/actions.js
@@ -26,7 +26,7 @@ const createHandlersMap = () => ({
         this.variables[varName] = value;
     },
     action_addVar(value, [ varName ]) {
-        if (this.variables[varName] === "undefined")
+        if (typeof this.variables[varName] === "undefined")
             return this.handlersMap.action_setVar(...arguments);
 
         this.variables[varName] += value;